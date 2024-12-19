6c4ffadb3aab179d449b0c787ef4f1a1d1456d42
“José <jose.luiz.guerra1@gmail.com>
2024-12-19 12:49:57 +0000

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
     environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
   },
 } as UserConfig)
